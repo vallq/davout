43de98dc1cfc932587d00abb15ceb38f24b83238
vallq <vallq@outlook.com>
2020-10-05 15:14:53 +0800

    eslint modified stats

--- src/components/DataStatistics.js
+++ src/components/DataStatistics.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import {Container} from 'semantic-ui-react';
-import { Table } from 'semantic-ui-react';
+import React from "react";
+import { Container } from "semantic-ui-react";
+import { Table } from "semantic-ui-react";
 
 const DataStatistics = (props) => {
-    const {data} = props;
+  const { data } = props;
 
-    return (
-      <Container >
-        <Table celled>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>min close</Table.HeaderCell>
-                <Table.HeaderCell>max close</Table.HeaderCell>
-                <Table.HeaderCell>mean close</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-                <Table.Row>
-                    <Table.Cell>{data.min}</Table.Cell>
-                    <Table.Cell>{data.max}</Table.Cell>
-                    <Table.Cell>{data.avg}</Table.Cell>
-                </Table.Row>
-            </Table.Body>
-          </Table>
-      </Container>
-)}
+  return (
+    <Container>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>min close</Table.HeaderCell>
+            <Table.HeaderCell>max close</Table.HeaderCell>
+            <Table.HeaderCell>mean close</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>{data.min}</Table.Cell>
+            <Table.Cell>{data.max}</Table.Cell>
+            <Table.Cell>{data.avg}</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </Container>
+  );
+};
 
 export default DataStatistics;
